a99b38b3a7b69d4571e6cceebca90b46c7f9478d
nerdrusso <92188332+nerdrusso@users.noreply.github.com>
2022-12-21 17:15:48 -0300

    Update fun.js

--- fun.js
+++ fun.js
@@ -1,22 +1,3 @@
-const resultsDiv = document.getElementById('results'); 
-
-   function bee() {
-    var list = JSON.parse(jsonInput.value);
-executeCommands(list);
-
-    }
-    function executeCommands(list) {
-  for (const item of list) {
-    if ('print' in item) {
-      console.log(item.print);
-    }else if ('printkk' in item) {
-      console.log(item.printkk + "kk");
-    }
- 
- 
- 
- 
- 
- 
-  }
+ function bee() {
+      console.log('YAYA');
 }
