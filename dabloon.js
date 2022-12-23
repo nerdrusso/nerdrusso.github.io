65ca78278bfc30ccfc006c5f6178cf1371d7b159
nerdrusso <92188332+nerdrusso@users.noreply.github.com>
2022-12-23 02:24:40 -0300

    Update dabloon.js

--- dabloon.js
+++ dabloon.js
@@ -1,63 +1,65 @@
+//it just logs, lol
 function log(txt) { 
     console.log(txt)
     }
-    
-   function id(name){ 
+
+
+//gets element by id
+function id(name){ 
 return document.getElementById(name);
 }
+function div(id, parent) {
+id = document.createElement('div');
+  element.id = id;
 
+  parent.appendChild(id);
+}
+
+
+//creates a buttom
 function btn(id, parent) {
 var elem = document.createElement('button');
   elem.id = id;
-  elem.border = 'none'
+
   parent.appendChild(elem);
 }
 
+
+//creates a div
 function div(id, parent) {
 var elem = document.createElement('div');
   elem.id = id;
 
   parent.appendChild(elem);
 }
-
-
-HTMLElement.prototype.bg = function(value) {
-  this.style.backgroundColor = value;
-}
-
-HTMLElement.prototype.txt = function(value) {
-  this.textContent = value;
-}
-
-function getTxt(element) {
-    return element.textContent;
-  }
-  
-  function getBg(element) {
-    return element.style.backgroundColor;
-  }
-
-
-HTMLElement.prototype.radius = function(radi) {
+ 
+//rounds the corners
+  HTMLElement.prototype.radiius = function(radi) {
   this.style.borderRadius = vh(radi);
 }
 
+//converts px to vh
+function vh(num)
+{
+    return num*0.14+"vh";
+}
 
 
+//sets the width
 HTMLElement.prototype.x = 
 function(xx) {
   this.style.width = vh(xx);
 }
 
+
+//sets the height
 HTMLElement.prototype.y = 
 function(yy) {
-if (yy.includes('%')){
-  this.style.height = (yy)
-} else {
-    this.style.height = vh(yy)
-}}
+  this.style.height = vh(yy);
+}
 
-function vh(num)
-{
-    return num*0.14+"vh";
-} 
+
+//sets click listener
+function onClick(element, func) {
+  element.addEventListener('click', func);
+}
